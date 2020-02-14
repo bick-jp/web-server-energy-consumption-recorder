2a976a3c04fc91a7cd58c8bd542455e894a77d53
Keita Ohshiro <keita.ohshiro@gmail.com>
2020-02-14 17:12:45 -0500

    make dir

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import threading
 import ina219_datarecorder
 import imagesize_pingpong
+import os
+import time
+import datetime
 
+folderName = str(datetime.date.today())+'-'+str(int(time.time()))
+newDirPath = 'data/'+ folderName
+os.mkdir(newDirPath)
+
+"""
 thread1 = threading.Thread(target=ina219_datarecorder.recordEnergyConsumption)
 thread2 = threading.Thread(target=imagesize_pingpong.pingpongImage)
 thread1.start()
@@ -12,3 +20,4 @@ thread2.join()
 print("main done")
 #ina219_datarecorder.recordEnergyConsumption()
 #imagesize_pingpong.pingpongImage()
+"""
